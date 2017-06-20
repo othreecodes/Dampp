f7dfd163a034f9ed417e9fc12946644b1bfcafc1
Obi Uchenna David <daviduchenna@outlook.com>
2017-06-20 05:40:39 +0100

    removed old pin lockview

--- app/src/main/java/com/davidmadethis/dampp/FollowUpFragment.java
+++ app/src/main/java/com/davidmadethis/dampp/FollowUpFragment.java
@@ -20,32 +20,11 @@ public class FollowUpFragment extends Fragment {
     String TAG = this.getClass().getName();
     public FollowUpFragment(){}
 
-    PinLockView mPinLockView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_followup, container, false);
 
-        mPinLockView = (PinLockView) v.findViewById(R.id.pin_lock_view);
-        mPinLockView.setPinLockListener(mPinLockListener);
-
         return v;
     }
-
-    private PinLockListener mPinLockListener = new PinLockListener() {
-        @Override
-        public void onComplete(String pin) {
-            Log.d(TAG, "Pin complete: " + pin);
-        }
-
-        @Override
-        public void onEmpty() {
-            Log.d(TAG, "Pin empty");
-        }
-
-        @Override
-        public void onPinChange(int pinLength, String intermediatePin) {
-            Log.d(TAG, "Pin changed, new length " + pinLength + " with intermediate pin " + intermediatePin);
-        }
-    };
 }
