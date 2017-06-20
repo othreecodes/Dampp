96f599e3ca995ddd4080df30fa49a870bbdb1f54
Obi Uchenna David <daviduchenna@outlook.com>
2017-06-20 06:05:03 +0100

    created loginview with lollipin

---
+++ app/src/main/java/com/davidmadethis/dampp/DamppAplication.java
@@ -0,0 +1,18 @@
+package com.davidmadethis.dampp;
+
+import android.app.Application;
+
+/**
+ * Created by root on 6/20/17.
+ */
+
+public class DamppAplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+
+
+}
--- app/src/main/java/com/davidmadethis/dampp/FollowUpFragment.java
+++ app/src/main/java/com/davidmadethis/dampp/FollowUpFragment.java
@@ -1,11 +1,15 @@
 package com.davidmadethis.dampp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.github.orangegangsters.lollipin.lib.managers.AppLock;
 
 /**
  * Created by root on 6/11/17.
@@ -15,12 +19,34 @@ public class FollowUpFragment extends Fragment {
 
     String TAG = this.getClass().getName();
     public FollowUpFragment(){}
-
+    int REQUEST_CODE_ENABLE = 333;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_followup, container, false);
 
+
+
+
         return v;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+
+        if(isVisibleToUser) {
+            Intent intent = new Intent(getActivity(), DamppPinActivity.class);
+            intent.putExtra(AppLock.EXTRA_TYPE, AppLock.ENABLE_PINLOCK);
+            startActivityForResult(intent, REQUEST_CODE_ENABLE);
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Toast.makeText(getActivity().getApplicationContext(),"GOTEN",Toast.LENGTH_LONG)
+                .show();
     }
 }
