611dcaf55fd2dbde7c569b5dd260c0d9b4ab2038
Obi Uchenna David <daviduchenna@outlook.com>
2017-07-09 15:20:35 +0100

    added CToast

---
+++ app/src/main/java/com/davidmadethis/dampp/custom/CToast.java
@@ -0,0 +1,14 @@
+package com.davidmadethis.dampp.custom;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by User on 7/3/2017.
+ */
+
+public class CToast extends Toast{
+    public CToast(Context context) {
+        super(context);
+    }
+}
